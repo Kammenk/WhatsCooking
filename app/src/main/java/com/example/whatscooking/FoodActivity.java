f61d869f04833693bf4bd4b1b29d921340a556a7
Kammenk <35629844+Kammenk@users.noreply.github.com>
2020-05-31 18:25:31 +0300

    first commit to dev branch

---
+++ app/src/main/java/com/example/whatscooking/FoodActivity.java
@@ -0,0 +1,16 @@
+package com.example.whatscooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food);
+
+        https://api.edamam.com/search?q=chicken&app_id=c2f84b80&app_key=73d477d26f4d7944598ac6e6332c992a
+    }
+}
--- app/src/main/java/com/example/whatscooking/MainActivity.java
+++ app/src/main/java/com/example/whatscooking/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.whatscooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,9 +24,25 @@ public class MainActivity extends AppCompatActivity {
         mainImageView = findViewById(R.id.mainImageView);
 
         setRandomBackground();
+        switchActivity();
 
 
 
+
+    }
+
+    public void switchActivity(){
+        Timer timer = new Timer();
+        int delay = 5000;
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, FoodActivity.class);
+                intent.putExtra("newActivity", "");
+                startActivity(intent);
+            }
+        },delay);
     }
 
     public void setRandomBackground(){
